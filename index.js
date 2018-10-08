737563af908c524323c9419313adc7cb84e1e0cd
ZhangZisu <admin@zhangzisu.cn>
2018-10-08 09:55:54 +0800

    Use prompts

--- index.js
+++ index.js
@@ -1,31 +1,41 @@
 const _ = require("lodash");
 const fs = require("fs");
 const problems = _.shuffle(JSON.parse(fs.readFileSync("bundle.json").toString()));
-const promptly = require("promptly");
+const prompts = require("prompts");
 
 (async () => {
-    const number = parseInt(await promptly.prompt("Length", {
-        validator: (value) => {
-            value = parseInt(value);
-            if (value < 1 || value > 25) throw new Error("Invalid input");
-            return value;
-        },
-        retry: true
-    }));
+    const number = (await prompts([
+        {
+            type: "number",
+            name: "number",
+            message: "Problem count",
+            initial: 5
+        }
+    ])).number;
     console.log(number);
+    const questions = [];
     for (let i = 0; i < number; i++) {
-        console.log(problems[i].description);
         problems[i].options = _.shuffle(problems[i].options);
         const choices = [];
-        for (let j = 0; j < problems[i].options.length; j++) {
-            console.log(`${j}: ${problems[i].options[j]}`);
-            choices.push(j);
+        for (let choice of problems[i].options) {
+            choices.push({ title: choice, value: choice });
         }
-        const result = await promptly.choose("Select one:", choices);
-        if (problems[i].options[result] === problems[i].answer) {
-            console.log("Correct");
-        } else {
-            console.log("Wrong");
+        questions.push(
+            {
+                type: "select",
+                name: i,
+                message: problems[i].description,
+                choices: choices
+            }
+        );
+    }
+    const result = await prompts(questions);
+    const failed = [];
+    for (let i in result) {
+        if (result[i] !== problems[i].answer) {
+            failed.push(problems[i]);
         }
     }
+    fs.writeFileSync("failed.json", JSON.stringify(problems, null, '\t'));
+    console.log(`AC Rate: ${number - failed.length} / ${number} ~~ ${(number - failed.length) / number}`);
 })();
